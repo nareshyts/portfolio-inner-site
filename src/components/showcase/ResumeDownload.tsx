c93f71ce67c5b29a16d6c3a1363c96b00299ad81
Mr perfect <154010359+nareshyts@users.noreply.github.com>
2026-05-31 06:14:55 +0530

    Update ResumeDownload.tsx

--- src/components/showcase/ResumeDownload.tsx
+++ src/components/showcase/ResumeDownload.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
+// REMOVED: import Resume from '../../assets/resume/Henry_Heffernan_S22_Resume.pdf';
 import printer from '../../assets/resume/printer.gif';
-
 
 export interface ResumeDownloadProps {
     altText?: string;
 }
 
 const ResumeDownload: React.FC<ResumeDownloadProps> = ({ altText }) => {
+    // This function does nothing - just prevents any action
+    const handleClick = () => {
+        // No action - you don't want to do anything
+        console.log("No resume available");
+    };
+
     return (
         <div style={styles.resumeContainer}>
             <img style={styles.resumePrinter} alt="" src={printer} />
             <div style={styles.resumeContainerText}>
                 <h3>{altText ? altText : 'Looking for my resume?'}</h3>
-                <a rel="noreferrer" target="_blank" href="#">
+                {/* Changed from <a> to <button> */}
+                <button 
+                    type="button"
+                    onClick={handleClick}
+                    style={styles.linkButton}
+                >
                     <p>u can't find cauz i don't have !</p>
-                </a>
+                </button>
             </div>
         </div>
     );
 };
 
-const styles: StyleSheetCSS = {
+const styles: { [key: string]: React.CSSProperties } = {
     resumeContainer: {
         backgroundColor: 'white',
         padding: 12,
@@ -38,6 +49,17 @@ const styles: StyleSheetCSS = {
         width: 56,
         height: 48,
         paddingRight: 24,
+    },
+    // Added style to make button look like a link
+    linkButton: {
+        background: 'none',
+        border: 'none',
+        padding: 0,
+        margin: 0,
+        cursor: 'pointer',
+        color: 'inherit',
+        font: 'inherit',
+        textDecoration: 'underline',
     },
 };
 
